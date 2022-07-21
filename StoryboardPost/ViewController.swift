8c43c486995955281dcf56f1d2683301b502a776
co.abu <anizomxoja@gmail.com>
2022-07-21 11:37:33 +0500

    Initial Commit

---
+++ StoryboardPost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardPost
+//
+//  Created by Avaz Mukhitdinov on 21/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
